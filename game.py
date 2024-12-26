77cb2b7235143f305eed1ed66c4d3f91ffd5aa13
Katya <accounteight@proton.me>
2024-12-26 15:53:34 +0300

    added game end and help function

--- game.py
+++ game.py
@@ -6,10 +6,22 @@ def start():
     my_points = []
     computer_points = []
     def comparisons():
+        if len(my_points) == 2:
+            print('You win! Game end!')
+            my_points.clear()
+            computer_points.clear()
+        elif len(computer_points) == 2:
+            print('Computer win! Game end!')
+            my_points.clear()
+            computer_points.clear()
+            comparisons()
         computer_choice = random.choice(['scissors','rock','paper'])
         my_choice = input('My choice: ')
-        """Draw"""
-        if my_choice == 'scissors' and computer_choice == 'scissors':
+        #HELP
+        if my_choice == '-help':
+            print('Game root:\n1. The game starts with your choice: rock, paper or scissors(nothing more).\n2. The game ends when one of the players reaches 10 points')
+        #DRAW
+        elif my_choice == 'scissors' and computer_choice == 'scissors':
             print(f'You: {my_choice}\nComputer: {computer_choice}\nResult: Draw\nME  COMPUTER\n{len(my_points)}      {len(computer_points)}')
         elif my_choice == 'rock' and computer_choice == 'rock':
             print(f'You: {my_choice}\nComputer: {computer_choice}\nResult: Draw\nME  COMPUTER\n{len(my_points)}      {len(computer_points)}')
@@ -37,6 +49,9 @@ def start():
         elif my_choice == 'rock' and computer_choice == 'paper':
             computer_points.append(1)
             print(f'You: {my_choice}\nComputer: {computer_choice}\nResult: Computer win!\nME  COMPUTER\n{len(my_points)}      {len(computer_points)}')
+        else:
+            print("Wrong command \nWrite -help for view more commands")
+            comparisons()
         comparisons()
     comparisons()
 start()
